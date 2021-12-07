e3f2334d713b4d148829481e677edc30830d3a03
mJaJksJ <maksim.m00@mail.ru>
2021-12-07 17:34:18 +0300

    интерпретатор (всё кроме if-else)

---
+++ Interpreter/Interpreter/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Interpreter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var text = File.ReadAllText("code.txt");
+
+            text = text.Replace("\n", " ");
+            text = text.Replace("\r", " ");
+            text = text.Replace("\t", " ");
+            text += " ";
+            while (text.Contains("  "))
+            {
+                text = text.Replace("  ", " ");
+            }
+            //var code = text.Split(" ", StringSplitOptions.RemoveEmptyEntries).ToList();
+            var code = new List<string>();
+            int i = 0;
+            string str = "";
+            while (true)
+            {
+                if(i >= text.Length)
+                {
+                    break;
+                }
+
+                if(str == "")
+                {
+                    str += text[i];
+                    i++;
+                    continue;
+                }
+
+                if(str.First() != '\"')
+                {
+                    while (text[i] != ' ')
+                    {
+                        str += text[i];
+                        i++;
+                    }
+                    code.Add(str);
+                    str = "";
+                    i++;
+                }
+                else
+                {
+                    while (text[i] != '\"')
+                    {
+                        str += text[i];
+                        i++;
+                    }
+                    str += text[i];
+                    i++;
+                    code.Add(str);
+                    str = "";
+                    i++;
+                }
+            }
+
+            int id = 0;
+            Interpreter.Program(code, ref id);
+
+        }
+    }
+}
